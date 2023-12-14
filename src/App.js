22bf28f132c95fc6bcbcbb5fac4d373300142169
Vishal Kumar <ironultimate321@gmail.com>
2023-12-14 17:02:19 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes,useNavigate, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
@@ -9,13 +9,15 @@ import { auth } from "./firebase-config";
 
 
 function App() {
+  const navigate=useNavigate()
   const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
 
   const signUserOut = () => {
     signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
-      window.location.pathname = "/login";
+      navigate('/login')
+      // window.location.pathname = "/login";
     });
   };
 
@@ -37,7 +39,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home isAuth={isAuth}/>} />
           <Route path="/CreatePost" element={<CreatePost isAuth={isAuth} />} />
-          <Route path="/Login" element={<Login setIsAuth={setIsAuth} />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         </Routes>
       </Router>
     </div>
